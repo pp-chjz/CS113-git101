d849383903a8f7aa6c91d7d71e212357d0badeb5
Pirapat <pirapat.j@ku.th>
2020-02-13 08:55:47 +0700

    Types,Operators,and Expressions

---
+++ week2.c
@@ -0,0 +1,21 @@
+//Types,Operators,and Expressions
+#include <stdio.h>
+#define bb "wow"
+float e;
+char r = '\0' ;
+int main()
+{
+   // printf("hello world\n");
+   int a=400;
+   char b='a';
+   char c=97;
+   float d = 0.134;
+   double f =20.198888;
+   // printf("%c %d %c %d\n",b,b,c,c);
+   // printf("%f %lf",f,d);
+   // printf("%f",e+1000);
+   // int ans1=a+b;
+   // float ans2=f+b;
+   float ans3 =11%15;
+   printf("%f",ans3);
+}
